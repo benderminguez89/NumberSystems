b505f2c74002339f9a184b1206e362eab634b954
Ben Dominguez <benderminguez@gmail.com>
2023-02-17 18:56:57 -0800

    binToDec and outDec now fully functional. Need to add error handling

--- src/Binary.java
+++ src/Binary.java
@@ -1,4 +1,5 @@
 import java.io.PrintWriter;
+import java.lang.String;
 import java.util.Scanner;
 import java.lang.StringBuilder;
 
@@ -7,15 +8,22 @@ public class Binary {
     private Scanner scan = new Scanner(System.in);
     private StringBuilder sb;
     private PrintWriter pw;
+    private String binNum;
+    private String hexNum;
+    private int decNum;
     public Binary(PrintWriter pw){
         this.pw = pw;
     }
     public void binToDec() {
         inputBin();
+        toDec();
+        outDec();
     }
 
     public void binToHex() {
         inputBin();
+        toHex();
+        outHex();
     }
 
 
@@ -23,15 +31,24 @@ public class Binary {
         System.out.println("Enter a String representing a binary number:" +
                 "\n\tNOTE:Binary numbers containing groupings of 0's and 1's" +
                 "\n\t\t The sequences should be in groups of 4\n\t - For example: 0011 is binary for 3");
-        String binNum = scan.nextLine();
+        binNum = scan.nextLine();
+        System.out.println("You entered: " + binNum);
 
     }
 
     private void toDec(){
-
+        int base = 1;
+        for(int length = binNum.length() - 1; length >= 0; length--){
+            if(binNum.charAt(length) == '1'){
+                decNum += base;
+            }
+            base = base * 2;
+        }
+        System.out.println(decNum);
     }
 
     private void outDec(){
+        pw.print(decNum);
 
     }
 
