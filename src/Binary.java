f75465b160f752a8cbc9477bf6245ae77372bf50
Ben Dominguez <benderminguez@gmail.com>
2023-02-17 19:18:15 -0800

    Added a test value and fixed formatting for pritwriter in the Binary class

--- src/Binary.java
+++ src/Binary.java
@@ -1,42 +1,60 @@
 import java.io.PrintWriter;
-import java.lang.String;
 import java.util.Scanner;
 import java.lang.StringBuilder;
+import java.lang.String;
 
 //Binary Class handles conversions from binary to other formats
 public class Binary {
     private Scanner scan = new Scanner(System.in);
     private StringBuilder sb;
     private PrintWriter pw;
+
     private String binNum;
     private String hexNum;
     private int decNum;
+    /*
+
+     */
     public Binary(PrintWriter pw){
         this.pw = pw;
+
     }
+
+    /*
+
+     */
     public void binToDec() {
         inputBin();
         toDec();
         outDec();
     }
 
+    /*
+
+     */
     public void binToHex() {
         inputBin();
         toHex();
         outHex();
     }
 
+    /*
 
+     */
     private void inputBin(){
+        binNum = "";
         System.out.println("Enter a String representing a binary number:" +
                 "\n\tNOTE:Binary numbers containing groupings of 0's and 1's" +
                 "\n\t\t The sequences should be in groups of 4\n\t - For example: 0011 is binary for 3");
         binNum = scan.nextLine();
         System.out.println("You entered: " + binNum);
-
     }
 
+    /*
+
+     */
     private void toDec(){
+        decNum = 0;
         int base = 1;
         for(int length = binNum.length() - 1; length >= 0; length--){
             if(binNum.charAt(length) == '1'){
@@ -47,16 +65,24 @@ public class Binary {
         System.out.println(decNum);
     }
 
+    /*
+
+     */
     private void outDec(){
-        pw.print(decNum);
-
+        pw.print("Binary to Decimal: " + decNum + "\n");
     }
 
+    /*
+
+     */
     private void toHex(){
-
+        hexNum = "TEST VAL";
     }
 
-    private void outHex() {
+    /*
 
+     */
+    private void outHex() {
+        pw.print("Binary to Hexidecimal: " + hexNum + "\n");
     }
 }
