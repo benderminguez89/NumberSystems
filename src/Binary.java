50abe1b123b95b18a89fbb91846e1e7bc1f50c7c
Ben Dominguez <benderminguez@gmail.com>
2023-02-20 19:01:49 -0800

    Binary functions are all functioning as expected

--- src/Binary.java
+++ src/Binary.java
@@ -1,4 +1,5 @@
 import java.io.PrintWriter;
+import java.util.Hashtable;
 import java.util.Scanner;
 import java.lang.StringBuilder;
 import java.lang.String;
@@ -17,7 +18,6 @@ public class Binary {
      */
     public Binary(PrintWriter pw){
         this.pw = pw;
-
     }
 
     /*
@@ -76,7 +76,32 @@ public class Binary {
 
      */
     private void toHex(){
-        hexNum = "TEST VAL";
+        Hashtable<String, String> binHex = new Hashtable<String, String>()
+        {{
+            put("0000","0");
+            put("0001","1");
+            put("0010","2");
+            put("0011","3");
+            put("0100","4");
+            put("0101","5");
+            put("0110","6");
+            put("0111","7");
+            put("1000","8");
+            put("1001","9");
+            put("1010","A");
+            put("1011","B");
+            put("1100","C");
+            put("1101","D");
+            put("1110","E");
+            put("1111","F");
+        }};
+
+        hexNum = "0x";
+
+        for(int i = 0; i < binNum.length()/4; i++){
+            hexNum += binHex.get(binNum.substring(i*4, (i*4)+4));
+        }
+        System.out.println(hexNum);
     }
 
     /*
